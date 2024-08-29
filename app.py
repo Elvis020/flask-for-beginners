1579264d20f6208ad3d42529a9815da729547e2b
Elvis020 <spaceyel3@gmail.com>
2024-08-29 09:25:25 +0000

    added delete functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
  
@@ -44,4 +44,18 @@ class Student(db.Model):
 @app.route("/")
 def index():
     return "<h1>Getting started with Flask and Databases</h1>"
- 
+
+
+@app.route("/students")
+def get_all_students():
+    students = Student.query.all()
+    return render_template('students.html',students=students)
+
+
+@app.route("/students/<string:name>")
+def delete_student(name):
+    Student.query.filter(Student.name == name).delete(synchronize_session=False)
+    remaining_students = Student.query.all()
+    db.session.add_all(remaining_students)
+    db.session.commit()
+    return render_template('students.html',students=remaining_students)
